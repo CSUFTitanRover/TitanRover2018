32d0d94ccc8f183fca340216a12d299bf74f6c0c
Weffe <rogelio_negrete@live.com>
2017-08-23 09:50:31 -0700

    Add golden-layout and deepstream config

--- user-interface/src/index.js
+++ user-interface/src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import GoldenLayout from 'golden-layout'
+import 'golden-layout/src/css/goldenlayout-base.css'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+// create skeleton layout
+const glLayout = new GoldenLayout({
+    content: [{
+        type: 'row',
+        content:[{
+            type:'react-component',
+            component: 'app-component',
+            props: { label: 'A' }
+        },{
+            type: 'column',
+            content:[{
+                type:'react-component',
+                component: 'app-component',
+                props: { label: 'B' }
+            },{
+                type:'react-component',
+                component: 'app-component',
+                props: { label: 'C' }
+            }]
+        }]
+    }]
+})  
+
+// register/associate react component with string id
+glLayout.registerComponent('app-component', App)
+
+//Once all components are registered, call
+glLayout.init()
+
+
+
