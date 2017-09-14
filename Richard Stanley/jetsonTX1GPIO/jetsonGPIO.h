efe37eca39d51b486eac7f44527300a14847b79c
Richard <audstanley@gmail.com>
2017-09-14 13:45:40 -0700

    got GPIO's WORKING!!

--- Richard Stanley/jetsonTX1GPIO/jetsonGPIO.h
+++ Richard Stanley/jetsonTX1GPIO/jetsonGPIO.h
@@ -63,32 +63,35 @@ enum pinValues {
     on = 1
 }  ;
 
-enum jetsonGPIONumber {
-    gpio57  =  57,    // J3A1 - Pin 50
-	gpio160 = 160,	  // J3A2 - Pin 40	
-	gpio161 = 161,    // J3A2 - Pin 43
-	gpio162 = 162,    // J3A2 - Pin 46
-	gpio163 = 163,    // J3A2 - Pin 49
-	gpio164 = 164,    // J3A2 - Pin 52
-	gpio165 = 165,    // J3A2 - Pin 55
-	gpio166 = 166     // J3A2 - Pin 58
-}  ;
+//enum jetsonGPIONumber {
+ //   gpio57  =  57,    // J3A1 - Pin 50
+//	gpio160 = 160,	  // J3A2 - Pin 40	
+//	gpio161 = 161,    // J3A2 - Pin 43
+//	gpio162 = 162,    // J3A2 - Pin 46
+//	gpio163 = 163,    // J3A2 - Pin 49
+//	gpio164 = 164,    // J3A2 - Pin 52
+//	gpio165 = 165,    // J3A2 - Pin 55
+//	gpio166 = 166     // J3A2 - Pin 58
+//}  ;
 
+//enum jetsonTX1GPIONumber {
+//      gpio36 = 36,      // J21 - Pin 32 - Unused - AO_DMIC_IN_CLK
+//       gpio37 = 37,      // J21 - Pin 16 - Unused - AO_DMIC_IN_DAT
+//       gpio38 = 38,      // J21 - Pin 13 - Bidir  - GPIO20/AUD_INT
+//       gpio63 = 63,      // J21 - Pin 33 - Bidir  - GPIO11_AP_WAKE_BT
+//       gpio184 = 184,    // J21 - Pin 18 - Input  - GPIO16_MDM_WAKE_AP
+//       gpio186 = 186,    // J21 - Pin 31 - Input  - GPIO9_MOTION_INT
+//       gpio187 = 187,    // J21 - Pin 37 - Output - GPIO8_ALS_PROX_INT
+//       gpio219 = 219,    // J21 - Pin 29 - Output - GPIO19_AUD_RST
+//} ;
 
-// NOTE FROM RICHARD STANLEY:
-// These pins will need to change.  I've alread changed the last one
-// pin 398... still need to change the others
 enum jetsonTX1GPIONumber {
-       gpio36 = 36,      // J21 - Pin 32 - Unused - AO_DMIC_IN_CLK
-       gpio37 = 37,      // J21 - Pin 16 - Unused - AO_DMIC_IN_DAT
-       gpio38 = 38,      // J21 - Pin 13 - Bidir  - GPIO20/AUD_INT
-       gpio63 = 63,      // J21 - Pin 33 - Bidir  - GPIO11_AP_WAKE_BT
-       gpio184 = 184,    // J21 - Pin 18 - Input  - GPIO16_MDM_WAKE_AP
-       gpio186 = 186,    // J21 - Pin 31 - Input  - GPIO9_MOTION_INT
-       gpio187 = 187,    // J21 - Pin 37 - Output - GPIO8_ALS_PROX_INT
-       gpio398 = 398,    // J21 - Pin 29 - Output - GPIO19_AUD_RST
+       gpio298 = 298,      // J21 - Pin 31 - Input  - GPIO9_MOTION_INT
+       gpio388 = 388,      // J21 - Pin 37 - Output - GPIO8_ALS_PROX_INT
+       gpio389 = 389,      // J21 - Pin 33 - Bidir  - GPIO11_AP_WAKE_BT
+       gpio398 = 398,      // J21 - Pin 29 - Output - GPIO19_AUD_RST
+       gpio481 = 481,      // J21 - Pin 18 - Input  - GPIO16_MDM_WAKE_AP
 } ;
-
 
 int gpioExport ( jetsonGPIO gpio ) ;
 int gpioUnexport ( jetsonGPIO gpio ) ;
