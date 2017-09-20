1c6f92dba061ea7ef9442be8ad26b0ebc715375a
Weffe <rogelio_negrete@live.com>
2017-09-20 15:19:14 -0700

    Create basic component files to be completed

---
+++ user-interface/src/components/Stopwatch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Stopwatch extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { time: 'some time value' }
+    }
+
+    render() {
+        return (
+            // TODO: render a stopwatch time that can be started/stopped/reset
+        )
+    }
+}
