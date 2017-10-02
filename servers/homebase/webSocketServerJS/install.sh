03f75a8b733e0d469c790812c6c8f9949f05dbea
Richard Stanley <audstanley@gmail.com>
2017-10-02 15:25:46 -0700

    Update install.sh

--- servers/homebase/webSocketServerJS/install.sh
+++ servers/homebase/webSocketServerJS/install.sh
@@ -7,7 +7,7 @@ else
         sudo apt-get install -y xboxdrv;
         echo "KERNEL==\"uinput\", MODE=\"0660\", GROUP=\"root\"" > /etc/udev/rules.d/55-permissions-uinput.rules;
 fi
-if [ "$(node -v)" != "v8.5.0" ]
+if [ "$(node -v)" != "v8.6.0" ]
         then echo "Current Version of node is already installed";
         wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | bash;
 else
