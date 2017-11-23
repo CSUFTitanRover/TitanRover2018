5ceaadadd2d9a1603c30f6da28be7ff035cfdc4d
Weffe <rogelio_negrete@live.com>
2017-11-23 03:53:02 -0800

    Add configuration for Deepstream to interact with mongoDB

--- rover/core/servers/Deepstream/ds-server/server.js
+++ rover/core/servers/Deepstream/ds-server/server.js
@@ -1,4 +1,5 @@
 const DeepstreamServer = require('deepstream.io');
+const MongoDBStorageConnector = require('deepstream.io-storage-mongodb');
 const C = DeepstreamServer.constants;
 /*
 The server can take
@@ -8,6 +9,12 @@ The server can take
 4) pass some options, missing options will be merged with the base configuration
 */
 const server = new DeepstreamServer('./ds-rover-config.yml');
+
+server.set('storage', new MongoDBStorageConnector({
+  connectionString: 'mongodb://127.0.0.1:27017/deepstream',
+  defaultCollection: 'deepstream_records',
+  splitChar: '/'
+}));
 
 // start the server
 server.start();
