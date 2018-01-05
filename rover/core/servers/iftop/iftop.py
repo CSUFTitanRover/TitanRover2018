4f978e9e7450f4e62f33699ad6c6ee925a7af67d
Richard Stanley <audstanley2@csu.fullerton.edu>
2018-01-05 15:11:56 -0800

    caught if deepstream is not online

--- rover/core/servers/iftop/iftop.py
+++ rover/core/servers/iftop/iftop.py
@@ -13,20 +13,26 @@ post(obj, recordName)
 while True:
     p = Popen([ "iftop", "-o", "10s", "-t", "-s", "10", "-L", "2", "-i", interface ], stdout=subprocess.PIPE)
     out, err = p.communicate()
-    uploadArr = re.findall(r"Total send rate:\s+(\d{1,}\.{0,1}\d{0,})(\w+)", out)
-    downloadArr = re.findall(r"Total receive rate:\s+(\d{1,}\.{0,1}\d{0,})(\w+)", out)
-    upload = 0
-    download = 0
-    if uploadArr is not [] and downloadArr is not []:
-        upload = float(uploadArr[0][0])
-        download = float(downloadArr[0][0])
-        if uploadArr[0][1] == "Kb":
-            upload = upload * 1000
-        if downloadArr[0][1] == "Kb":
-            download = download * 1000
-        if uploadArr[0][1] == "Mb":
-            upload = upload * 1000000
-        if downloadArr[0][1] == "Mb":
-            download = download * 1000000
-        print "Upload: {} {} Download: {} {}".format(upload, uploadArr[0][1], download , downloadArr[0][1])
-        post({"upload": upload, "download": download}, recordName)
+    try:
+        uploadArr = re.findall(r"Total send rate:\s+(\d{1,}\.{0,1}\d{0,})(\w+)", out)
+        downloadArr = re.findall(r"Total receive rate:\s+(\d{1,}\.{0,1}\d{0,})(\w+)", out)
+        upload = 0
+        download = 0
+        if uploadArr is not [] and downloadArr is not []:
+            upload = float(uploadArr[0][0])
+            download = float(downloadArr[0][0])
+            if uploadArr[0][1] == "Kb":
+                upload = upload * 1000
+            if downloadArr[0][1] == "Kb":
+                download = download * 1000
+            if uploadArr[0][1] == "Mb":
+                upload = upload * 1000000
+            if downloadArr[0][1] == "Mb":
+                download = download * 1000000
+            print "Upload: {} {} Download: {} {}".format(upload, uploadArr[0][1], download , downloadArr[0][1])
+            post({"upload": upload, "download": download}, recordName)
+    except:
+        try:
+            post({}, "speed")
+        except:
+            print("cannot connect to deepstream.")
