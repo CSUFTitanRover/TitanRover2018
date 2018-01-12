a2d39f7f5bfeaeaa6de674a076ed10a478d33fbe
rawalshree <rawalshreepal000@gmail.com>
2018-01-12 15:49:47 -0800

    updated correction input script

--- rover/controls-systems/mobility/GNSS/socketServer.py
+++ rover/controls-systems/mobility/GNSS/socketServer.py
@@ -3,7 +3,6 @@ from time import sleep
 import subprocess
 import serial
 
-#i = 1
 
 def startListening():
     while True:
@@ -14,12 +13,6 @@ def startListening():
             print("Not Connected to the Reach on /dev/ttyACM*")
             sleep(1)
 
-            #counter = 0
-            #pid = subprocess.call(["sudo", "lsof", "-t", "-i", "tcp:9000"])
-            #while counter < 5:
-                #subprocess.call(' sudo lsof -t -i tcp:9000 | xargs kill -9', shell = True)
-                #counter += 1
-            
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 9000))
     s.listen(5)
@@ -45,15 +38,6 @@ def startListening():
             #i = 0
             print("Connection Terminated")
             break
-        '''
-        except:
-            print("Not Connected to the Reach on /dev/ttyACM*")
-            sleep(1)
-            #if i is 0:
-            #subprocess.call(["python", "socketServer.py"])
-            #i = 1
-        '''
-
 
 while True:
     try:
