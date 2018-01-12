1bab4b55263e2fe49253ba6da57f957032757a1e
rawalshree <rawalshreepal000@gmail.com>
2018-01-12 15:12:41 -0800

    minor update

--- rover/controls-systems/mobility/GNSS/usb0ip.py
+++ rover/controls-systems/mobility/GNSS/usb0ip.py
@@ -1,12 +1,7 @@
-import netifaces as ni
 from subprocess import Popen, PIPE
 from time import sleep
 
 
-
 while True:
-    try:
         Popen(["ifconfig", "usb0", "192.168.2.2"])
         sleep(5)
-    except:
-        print("No Such Device")
