9a940f32fd4de1d254464e13a3b89da589adef98
Can <Jayteare@users.noreply.github.com>
2018-02-13 16:26:17 -0800

    Update slamMaze.cpp

--- rover/controls-systems/SLAM/slamMaze.cpp
+++ rover/controls-systems/SLAM/slamMaze.cpp
@@ -1,6 +1,7 @@
 // SLAM-Basic-Maze.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
 #include "GL\glew.h"
 #include "GL\freeglut.h"
 #include <string>
@@ -8,17 +9,14 @@
 #include <iostream>
 #include <conio.h>
 #include <vector>
+#include <atomic>
+#include <mutex>
+#define WINVER 0x0500
+#include <windows.h>
 
-//void _openGLBuffers()
-//{
-//	GLuint bufferID;
-//	// Generate a buffer ID
-//	glGenBuffers(1, &bufferID);
-//	// Make this the current UNPACK buffer (OpenGL is state-based)
-//	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, bufferID);
-//	// Allocate data for the buffer
-//	glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * 4,NULL, GL_DYNAMIC_COPY);
-//}
+#pragma region Global_Data_Objects
+
+std::mutex mtx;
 
 struct _openGL_CAMERA_VALUES_
 {
@@ -66,12 +64,21 @@ struct _openGL_ROVER_LINE_TRACKING_
 	std::vector<double> line_z;
 }_openGLRLT_;
 
-void setProjection(int w1, int h1)
+#pragma endregion Global_Data_Objects
+
+
+std::vector<GLfloat> past_x;
+std::vector<GLfloat> past_y;
+std::vector<GLfloat> past_z;
+
+void setProjection(GLint w1, GLint h1)
 {
-	float ratio;
+	mtx.lock();
+	std::atomic<GLfloat> ratio;
+	ratio.store(0, std::memory_order_release);
 	// Prevent a divide by zero, when window is too short
 	// (you cant make a window of zero width).
-	ratio = 1.0f * w1 / h1;
+	ratio.store((1.0f * w1 / h1),std::memory_order_relaxed);
 	// Reset the coordinate system before modifying
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -80,24 +87,25 @@ void setProjection(int w1, int h1)
 	glViewport(0, 0, w1, h1);
 
 	// Set the clipping volume
-	gluPerspective(45, ratio, 0.1, 1000);
+	gluPerspective(45, ratio.load(), 0.1, 1000);
 	glMatrixMode(GL_MODELVIEW);
+	mtx.unlock();
 }
 
-void changeSize(int w1, int h1) 
+void changeSize(GLint w1, GLint h1) 
 {
 	if (h1 == 0){ h1 = 1; }
 		
 	// we're keeping these values cause we'll need them latter
-
-	_openGLMV_.width = w1;
+	_openGLMV_.width  = w1;
 	_openGLMV_.height = h1;
 
 	// set topdownWindow as the active window
 	glutSetWindow(_openGLMV_.TopDownWindow);
 	// resize and reposition the sub window
-	glutPositionWindow(_openGLMV_.border, (_openGLMV_.height + _openGLMV_.border) / 2);
-	glutReshapeWindow(_openGLMV_.width / 2 - _openGLMV_.border * 3 / 2, _openGLMV_.height / 2 - _openGLMV_.border * 3 / 2);
+	glutPositionWindow(_openGLMV_.border, (_openGLMV_.height + _openGLMV_.border) / 8);
+	//glutReshapeWindow(_openGLMV_.width / 2 - _openGLMV_.border * 3 / 2, _openGLMV_.height / 2 - _openGLMV_.border * 3 / 2);
+	glutReshapeWindow(_openGLMV_.width-20, _openGLMV_.height-20);
 	setProjection(_openGLMV_.width / 2 - _openGLMV_.border * 3 / 2, _openGLMV_.height / 2 - _openGLMV_.border * 3 / 2);
 }
 //---------------------------------------------------------------------------------
@@ -106,66 +114,53 @@ void changeSize(int w1, int h1)
 //       STATIC RENDERING OBJECTS
 // --------------------------------------------------------------------------------
 //Static Drawcube object for rendering.
+#pragma region Static_Objects
+
 void drawCircle()
 {
-	glColor3f(1.0, 0.0, 1.0); // Color Purple
+	mtx.lock();
+	glColor3f(0.0, 1.0, 0.0); // <R,G,B>
 
 	//Circle
 	glTranslatef(0.0f, 0.75f, 0.0f);
-	glutSolidSphere(0.15f, 20, 20);
+	glutSolidSphere(0.15f, 10, 10);
+	mtx.unlock();
+}
+
+void drawTennisBall()
+{
+	mtx.lock();
+	glColor3f(1.0, 1.0, 0.0); // <R,G,B>
+
+	//Circle
+	glTranslatef(0.0f, 0.75f, 0.0f);
+	glutSolidSphere(0.10f, 5, 5);
+	mtx.unlock();
 }
 
 //Static Drawcube object for rendering.
 void drawCube() 
 {
 	//Generic cube size
-	double cube_size = 0.6;
-
-	// Purple side - BACK
-	//glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 1.0); // Color representation of the polygon section.
-	//glVertex3f(cube_size, -cube_size, cube_size);
-	//glVertex3f(cube_size, cube_size, cube_size);
-	//glVertex3f(-cube_size, cube_size, cube_size);
-	//glVertex3f(-cube_size, -cube_size, cube_size);
-	//glEnd();
-
-	// Purple side - RIGHT
-	//glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 1.0);
-	//glVertex3f(cube_size, -cube_size, -cube_size);
-	//glVertex3f(cube_size, cube_size, -cube_size);
-	//glVertex3f(cube_size, cube_size, cube_size);
-	//glVertex3f(cube_size, -cube_size, cube_size);
-	//glEnd();
-
-	// Purple side - LEFT
-	//glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 1.0);
-	//glVertex3f(-cube_size, -cube_size, cube_size);
-	//glVertex3f(-cube_size, cube_size, cube_size);
-	//glVertex3f(-cube_size, cube_size, -cube_size);
-	//glVertex3f(-cube_size, -cube_size, -cube_size);
-	//glEnd();
+	mtx.lock();
+	std::atomic<GLdouble> cube_size;
+	cube_size.store(0.10,std::memory_order_relaxed);
+	mtx.unlock();
 
 	// Red side - TOP
+	mtx.lock();
 	glBegin(GL_POLYGON);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(cube_size, cube_size, cube_size);
+	glVertex3f(cube_size.load(), cube_size.load(), cube_size.load());
 	glVertex3f(cube_size, cube_size, -cube_size);
 	glVertex3f(-cube_size, cube_size, -cube_size);
 	glVertex3f(-cube_size, cube_size, cube_size);
 	glEnd();
-
-	//glBegin(GL_POLYGON);
-	//glColor3f(1.0, 0.0, 0.0);
-	//glVertex3f(cube_size, -cube_size, -cube_size);
-	//glVertex3f(cube_size, -cube_size, cube_size);
-	//glVertex3f(-cube_size, -cube_size, cube_size);
-	//glVertex3f(-cube_size, -cube_size, -cube_size);
-	//glEnd();
+	mtx.unlock();
 }
-//END OF DRAW CUBE
+
+#pragma endregion Static_Objects
+
+//END OF STATIC OBJECTS
 //----------------------------------------------------------------------------------
 
 void renderBitmapString(
@@ -177,7 +172,7 @@ void renderBitmapString(
 {
 
 	char *c;
-	glRasterPos3f(x, y, z);
+	glRasterPos2i(x, y);
 	for (c = string; *c != '\0'; c++)
 	{
 		glutBitmapCharacter(font, *c);
@@ -193,7 +188,6 @@ void restorePerspectiveProjection()
 	// get back to modelview mode
 	glMatrixMode(GL_MODELVIEW);
 }
-
 
 void setOrthographicProjection() 
 {
@@ -214,8 +208,55 @@ void setOrthographicProjection()
 	glMatrixMode(GL_MODELVIEW);
 }
 
+// Common Render Items for all subwindows
+void renderSubWindowScene() 
+{
+	mtx.lock();
+	std::atomic<GLdouble> plain_size;
+	std::atomic<GLdouble> ground_level;
+	std::atomic<GLdouble> object_pos;
 
-void computePos(GLfloat deltaMove) 
+	plain_size.store(100.0f, std::memory_order_relaxed);
+	ground_level.store(0.0f, std::memory_order_relaxed);
+	object_pos.store(10.0f, std::memory_order_relaxed);
+	mtx.unlock();
+
+	// Draw ground
+	mtx.lock();
+	glColor3f(0.647059f, 0.164706f, 0.164706f); //<R,G,B>
+
+	GLuint tex;
+	glGenTextures(1, &tex);
+
+	glBegin(GL_QUADS);
+	glVertex3f(-plain_size, ground_level, -plain_size);
+	glVertex3f(-plain_size, ground_level, plain_size);
+	glVertex3f(plain_size, ground_level, plain_size);
+	glVertex3f(plain_size, ground_level, -plain_size);
+	glEnd();
+	mtx.unlock();
+
+	// Draw and placement.
+	std::atomic<int> i;
+	#pragma omp parrallel
+	{
+		for (i.store(-3, std::memory_order_relaxed); i.load() < 3; i++)
+		{
+			#pragma omp for
+			for (std::atomic<int> j = -3; j.load() < 3; j++)
+			{
+				glPushMatrix();
+				glColor3f(1.0, 0.0, 0.0); //<R,G,B>
+				glTranslatef(i.load() * object_pos.load(), 0.0f, j.load() * object_pos.load());
+				drawCube();
+				glPopMatrix();
+			}
+		}
+	}
+
+}
+
+void computePos(GLfloat deltaMove)
 {
 	_openGLCV_.x += deltaMove * _openGLCV_.lx * 0.1f;
 	_openGLCV_.z += deltaMove * _openGLCV_.lz * 0.1f;
@@ -227,7 +268,7 @@ void computePos(GLfloat deltaMove)
 	std::cout << "z pos = " << _openGLCV_.z << std::endl;
 }
 
-void computeDir(GLfloat deltaAngle) 
+void computeDir(GLfloat deltaAngle)
 {
 	_openGLCV_.angle += deltaAngle;
 	_openGLCV_.lx = sin(_openGLCV_.angle);
@@ -236,35 +277,6 @@ void computeDir(GLfloat deltaAngle)
 	std::cout << "angle = " << _openGLCV_.angle << std::endl;
 	std::cout << "lx = " << _openGLCV_.lx << std::endl;
 	std::cout << "lz = " << _openGLCV_.lz << std::endl;
-}
-
-// Common Render Items for all subwindows
-void renderSubWindowScene() 
-{
-	double plain_size   = 100.0f;
-	double ground_level = 0.0f;
-
-	// Draw ground
-	glColor3f(0.9f, 0.9f, 0.9f); // Gray-White
-	glBegin(GL_QUADS);
-	glVertex3f(-plain_size, ground_level, -plain_size);
-	glVertex3f(-plain_size, ground_level, plain_size);
-	glVertex3f(plain_size, ground_level, plain_size);
-	glVertex3f(plain_size, ground_level, -plain_size);
-	glEnd();
-
-	// Draw and placement.
-	double object_pos = 10.0f;
-	for (int i = -3; i < 3; i++)
-	{
-		for (int j = -3; j < 3; j++)
-		{
-			glPushMatrix();
-			glTranslatef(i * object_pos, 0.0f, j * object_pos);
-			drawCube();
-			glPopMatrix();
-		}
-	}
 }
 
 // Display function for main window.
@@ -282,18 +294,43 @@ void renderTopDownScene()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	int index = 0;
+	std::atomic<int> index;
+	index.store(0,std::memory_order_relaxed);
 
 	//Main Camera Position, starting position for top down view.
 	gluLookAt(_openGLCV_.x, _openGLCV_.y + 15, _openGLCV_.z, _openGLCV_.x,
-	_openGLCV_.y - 1, _openGLCV_.z, _openGLCV_.lx, 0, _openGLCV_.lz);
+		_openGLCV_.y - 1, _openGLCV_.z, _openGLCV_.lx, 0, _openGLCV_.lz);
 
 	// Draw red cone at the location of the main camera.
 	glPushMatrix();
-	glColor3f(1.0, 0.0, 0.0);
+	glColor3f(0.0, 0.0, 1.0); //<R,G,B>
 	glTranslatef(_openGLCV_.x, _openGLCV_.y, _openGLCV_.z);
 	glRotatef(180 - (_openGLCV_.angle + _openGLKS_.deltaAngle)*180.0 / 3.14, 0.0, 1.0, 0.0);
-	glutSolidCone(0.2, 0.8f, 4, 4);
+	glutSolidCone(0.2, 0.5f, 2, 2);
+	glPopMatrix();
+
+	//Grabs the Current location of the main object.
+	GLfloat matrixMV[16];
+	glGetFloatv(GL_MODELVIEW_MATRIX, matrixMV);
+	GLdouble xCenterTriangle = matrixMV[12];
+	GLdouble yCenterTriangle = matrixMV[13];
+	GLdouble zCenterTriangle = matrixMV[14];
+
+	//Renders a static tennis ball
+	glPushMatrix();
+	glTranslatef(1.6, 0.0f, -1.6);
+	past_x.push_back(1.6);
+	past_y.push_back(0.0f);
+	past_z.push_back(-1.6);
+
+	if(!_openGLRT_.past_x.empty())
+		//glTranslatef(_openGLCV_.x, _openGLCV_.y, _openGLCV_.z);
+	glColor3f(1.0, 1.0, 0.0); //<R,G,B>
+	drawTennisBall();
+
+	std::string temp_x = " Tennis Ball";
+	char *cstr_x = &temp_x[0u];
+	renderBitmapString(1.6, 0.0f, -1.6, GLUT_BITMAP_HELVETICA_12, cstr_x);
 	glPopMatrix();
 
 	for (int i = 0; i < _openGLRT_.past_x.size(); i++)
@@ -305,21 +342,28 @@ void renderTopDownScene()
 
 			glPushMatrix();
 			glTranslatef(_openGLRT_.past_x[i], 0.0f, _openGLRT_.past_z[i]);
+			glColor3f(1.0, 0.0, 0.0); //<R,G,B>
+			std::string temp_x = " x: " + std::to_string(_openGLRT_.past_x[i]) + " y: " + std::to_string(_openGLRT_.past_z[i]);
+			char *cstr_x = &temp_x[0u];
+			renderBitmapString(0, 0.0f, 0, GLUT_BITMAP_HELVETICA_12, cstr_x);
+			
 			drawCircle();
 			glPopMatrix();
 			// limit the display trail and pass it to another tracking function.
 		}
 	}
 
+	//Draws the tracer red-line from the first point of view.
+	//Needs a better rework of the y direction calculation between matrices.
 	for (int i = 1; i < _openGLRLT_.line_x.size(); i++)
 	{
-		//P2 = P1 + V;
 		glPushMatrix();
-		glLineWidth(1.5);
-		glColor3f(1.0, 0.0, 0.0);
+		glLineWidth(1.0);
+		glColor3f(1.0, 0.0, 0.0); //<R,G,B>
 		glBegin(GL_LINES);
 		glVertex3f(_openGLRLT_.line_x[i-1], 0.0f, _openGLRLT_.line_z[i-1]);
-		glVertex3f(_openGLRLT_.line_x[i], 0.0f , _openGLRLT_.line_z[i]);
+		glColor3f(0.0, 0.0, 1.0); //<R,G,B>
+		glVertex3f(_openGLRLT_.line_x[i], 0.0f, _openGLRLT_.line_z[i]);
 		glEnd();
 		glPopMatrix();
 	}
@@ -342,7 +386,6 @@ void renderSceneAll()
 	}
 
 	// Left right movement reference check.
-
 	if (_openGLKS_.deltaAngle)
 	{
 		computeDir(_openGLKS_.deltaAngle);
@@ -358,7 +401,7 @@ void renderSceneAll()
 // --------------------------------------------------------------------------------
 //             KEYBOARD SECTION
 // --------------------------------------------------------------------------------
-
+#pragma region Keyboard_Section
 
 void processNormalKeys(unsigned char key, GLint xx, GLint yy) {
 
@@ -371,6 +414,7 @@ void processNormalKeys(unsigned char key, GLint xx, GLint yy) {
 
 void pressKey(GLint key, GLint xx, GLint yy) 
 {
+
 	switch (key) 
 	{
 		case GLUT_KEY_LEFT: _openGLKS_.deltaAngle  = -0.01f; break;
@@ -383,6 +427,56 @@ void pressKey(GLint key, GLint xx, GLint yy)
 	glutPostRedisplay();
 }
 
+void simulate_automation()
+{
+	// input event.
+	INPUT ip;
+
+	// Set up a generic keyboard event.
+	ip.type = INPUT_KEYBOARD;
+	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.time = 0;
+	ip.ki.dwExtraInfo = 0;
+
+	if (!past_x.empty())
+	{
+		if (_openGLCV_.z > past_z[0])
+		{
+			// Press the "Up" key
+			ip.ki.wVk = 0x26; // virtual-key code for the "Up" key
+			ip.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip, sizeof(INPUT));
+		}
+
+		if (_openGLCV_.z < past_z[0])
+		{
+			// Press the "Up" key
+			ip.ki.wVk = 0x28; // virtual-key code for the "Up" key
+			ip.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip, sizeof(INPUT));
+		}
+
+		if (_openGLCV_.x > past_x[0])
+		{
+			// Press the "Up" key
+			ip.ki.wVk = 0x25; // virtual-key code for the "Up" key
+			ip.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip, sizeof(INPUT));
+
+		}
+
+		if (_openGLCV_.x < past_x[0])
+		{
+			// Press the "Up" key
+			ip.ki.wVk = 0x27; // virtual-key code for the "Up" key
+			ip.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip, sizeof(INPUT));
+		}
+	}
+
+
+}
+
 void releaseKey(int key, int x, int y) 
 {
 	switch (key) 
@@ -392,12 +486,19 @@ void releaseKey(int key, int x, int y)
 		case GLUT_KEY_UP: std::cout << "Up Key Pressed" << std::endl; break;
 		case GLUT_KEY_DOWN: std::cout << "Down Key Pressed" << std::endl; _openGLKS_.deltaMove = 0; break;
 	}
+
+	//Can automate the guidance cycle here:
+	//
+	//---------
+	//simulate_automation();
 }
+
+#pragma endregion Keyboard_Section
 
 // --------------------------------------------------------------------------------
 //             MOUSE INPUT SECTION
 // --------------------------------------------------------------------------------
-
+#pragma region Mouse_Section
 
 void physicalCameraMove(int x, int y)
 {
@@ -437,6 +538,8 @@ void mouseButton(int button, int state, int x, int y)
 	}
 }
 
+#pragma endregion Mouse_Section
+
 // --------------------------------------------------------------------------------
 //             MAIN and INIT
 // --------------------------------------------------------------------------------
@@ -472,12 +575,12 @@ int main(int argc, char **argv)
 	glutDisplayFunc(renderSceneAll);
 	glutReshapeFunc(changeSize);
 
-	//init();
+	init();
 
 	//-----------------------------------------------------------------------------
 	//Top down view window decleration.
 	std::cout << "Registering callbacks for Top Down started..." << std::endl;
-	_openGLMV_.TopDownWindow = glutCreateWindow("SLAM MAZE");
+	_openGLMV_.TopDownWindow = glutCreateSubWindow(_openGLMV_.mainWindow, 10, 10, 350, 350);
 	glutDisplayFunc(renderTopDownScene);
 
 	init();
