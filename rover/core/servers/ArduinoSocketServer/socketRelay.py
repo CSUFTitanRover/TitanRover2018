27aba605d1a3881e6eba86ecb51089f30184df90
Richard Stanley <audstanley@gmail.com>
2018-03-05 07:59:23 -0800

    socket relay and deepstream update

--- rover/core/servers/ArduinoSocketServer/socketRelay.py
+++ rover/core/servers/ArduinoSocketServer/socketRelay.py
@@ -5,22 +5,28 @@
     Connect to serial for a 433.000Mhz connection to our mobility code,
     and to accept a connection from a 433.400Mhz relay if we want to double the distance of our rover.
 """
-
+import select
 from threading import Thread
-from deepstream import get
+from deepstream import get, post
+from subprocess import Popen, PIPE
 from time import sleep
 import socket
 import sys
 from relayFunctions import ep
 import re
-import serial
+from serial import Serial
 import os
 
-regex = r"([a-z])(-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,}),(\d{1,}\.?\d{1,}?)#"
+regex = r"([a-z])(-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,},-?\d{1,}),(-?\d{1,}\.?\d{1,}?)#"
 
-ghzMessageAlpha   = [ 'a', '0,0,0,0,0,0,0,0,0,0', str(ep()) ]
-hamMessageBravo   = [ 'b', '0,0,0,0,0,0,0,0,0,0', str(ep()) ]
-hamMessageCharlie = [ 'c', '0,0,0,0,0,0,0,0,0,0', str(ep()) ]
+initialTimeStamp = str(ep())
+ghzMessageAlpha   = [ 'a', '0,0,0,0,0,0,0,0,0,0', initialTimeStamp ]
+hamMessageBravo   = [ 'b', '0,0,0,0,0,0,0,0,0,0', initialTimeStamp ]
+hamMessageCharlie = [ 'c', '0,0,0,0,0,0,0,0,0,0', initialTimeStamp ]
+secondsOffset = 0
+
+# SET THE MODE TO MANUAL TEMPORARILY
+# post({"mode": "manual"}, "mode")
 
 mode = {}
 
@@ -38,129 +44,199 @@ def filterData(s):
     else:
       return ""
 
-# Ghz Socket Connection
+
+
+
+
+
+"""
+
+  Ghz Socket Connection
+
+"""
 def connectionToGhzAlpha():
-    # Create a TCP/IP socket for the base
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Bind the socket to the port
-    server_address = ('localhost', 5004)
-    print >>sys.stderr, 'starting up on %s port %s' % server_address
-    sock.bind(server_address)
+    global ghzMessageAlpha, secondsOffset
 
-    global ghzMessageAlpha
     while True:
+        #print("GHZ LOOP")
         d, address = sock.recvfrom(512)
-        #print(d)
-
+        d = str(d.decode('utf-8'))
+        #print("Directly from mobility:", d)
         if d:
+            try:
+              fMessage = filterData(d)
+              #print("DataFromMobility", fMessage.group(2))
+              if fMessage != '':
+                ghzMessageAlpha = [ fMessage.group(1), fMessage.group(2), fMessage.group(3) ]
+                secondsOffset = ep() - float(ghzMessageAlpha[2])
+            except:
+                print("could not parse the regex")
+                pass
+
             try:
               sent = sock.sendto('r', address)
             except:
               print("couldn't send message back to base")
+        else:
+          print("Ghz data failed")
 
-            try:
-              fMessage = filterData(d)
-              if fMessage != '':
-                ghzMessageAlpha = [ fMessage.group(1), fMessage.group(2), fMessage.group(3) ]
-                #print("Ghz:", ghzMessage)
-            except:
-                pass
 
-# Ham Serial Connection - bravo (433Mhz, channel 0)
+
+"""
+
+  Ham Serial Connection - bravo (433Mhz, channel 0)
+
+"""
+
+try:
+  ser = Serial('/dev/serial/by-id/usb-Silicon_Labs_titan_rover_1002-if00-port0', 9600, timeout=None)
+except:
+  print("cannot connect to serial device")
+  pass
+
 def connectionToHamBravo():
+    global hamMessageBravo
     consecutiveMessages = ['-', '-']
     mod2 = 1
-    global hamMessageBravo
+
     while True:
         mod2 = (mod2 + 1) % 2
         try:
-          ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
-        except:
-          #print("cannot connect to serial device")
-          pass
-        
-        try:
           message = ser.read_all()
-          print(message)
+          message = message.decode('utf-8')
+          print("New Ham Message: " + message)
           try:
             fMessage = filterData(message)
             if fMessage != "":
-                hamMessageBravo = fMessage
-                print(hamMessageBravo)
+                hamMessageBravo = [ fMessage.group(1), fMessage.group(2), fMessage.group(3) ]
+                #print("successful Ham parse:" + hamMessageBravo[1])
                 #print("Ham:", hamMessage)
-            else:
-                consecutiveMessages[mod2] = message
+            elif mod2 == 0:
+                consecutiveMessages[1] = message
                 fMessage = filterData(consecutiveMessages[0] + consecutiveMessages[1])
-                print(hamMessageBravo)
-                if fMessage != "" and mod2 == 1:
-                    hamMessageBravo = fMessage
+                print("consecutive string: " + hamMessageBravo)
+                if fMessage != "":
+                    hamMessageBravo = [ fMessage.group(1), fMessage.group(2), fMessage.group(3) ]
+            elif mod2 == 1:
+                consecutiveMessages[0] = message
+                fMessage = filterData(consecutiveMessages[1] + consecutiveMessages[0])
+                print("consecutive string: " + hamMessageBravo)
+                if fMessage != "":
+                    hamMessageBravo = [ fMessage.group(1), fMessage.group(2), fMessage.group(3) ] 
+                    
           except:
+            #print("something went wrong with parsing")
             pass
-        
-
         
         except:
             #print("could not read serial device")
             pass
-        sleep(0.4)
+        sleep(0.08)
 
 
 def sendLatestMessage():
+    zed = [ 'z', '0,0,0,0,0,0,0,0,0,0', str(ep()) ]
     # Create a TCP/IP socket to the arduino
     sockArd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #sockArd.setblocking(0)
+    #ready = select.select([sockArd], [], [], 1) 
     # Bind the socket to the port
     arduino_address = ('192.168.1.10', 5000)
+    data = "0,0,0,0,0,0,0,0,0,0"
     #sockArd.bind(arduino_address)
-    sockArd.settimeout(0.25)
-    global ghzMessageAlpha, hamMessageBravo, mode
+    sockArd.settimeout(10)
+    global ghzMessageAlpha, hamMessageBravo, mode, secondsOffset
+    #while True:
     try:
-        sockArd.sendto('0,0,0,0,0,0,0,0,0,0', arduino_address)
+      sockArd.sendto(data, arduino_address)
+      #print(data)
     except:
-        print("Could not make initial connection to the arduino...")
+      print("Could not make initial connection to the arduino...")
+    
     while True:
-        # Ternary operator to check the time stamp of the message
-        message = ghzMessageAlpha if ep() - float(ghzMessageAlpha[2]) < ep() - float(hamMessageBravo[2]) else hamMessageBravo
+        # Ternary operator to check the time stamp of the message.
+        # The greater the number (timestamp), the more recent the timestamp.
+        message = ghzMessageAlpha if float(ghzMessageAlpha[2]) > float(hamMessageBravo[2]) else hamMessageBravo
         # Ternary operator to check if elapsed time is greater than 2 seconds
-        print("messageTimeDiff:", ep() - float(message[2]))
-        message = message if ep() - float(message[2]) < 2 else ['not receiving data from anything', '0,0,0,0,0,0,0,0,0,0', str(ep() + 10)]
-        
-        print('Message:', message)
+        #print("DiffBetweenGhzAndHam:", ep () - float(message[2]) - secondsOffset)
+        #message = message if ep() - float(message[2]) - secondsOffset < 3 else zed
+        #print(ghzMessageAlpha[2])
+        d = None
+        #print("waiting for 'r'")
         try:
-            d = sockArd.recvfrom(512)[0]
+          d = sockArd.recvfrom(512)[0]
+          if d == 'r':
+            #print("ARDUINO IS RESPONDING")
+            pass
+          else:
+            #print("ARDUINO IS NOT RESPONDING")
+            pass
         except:
             d = None
-            print("Did not get a message back from the arduino")
+            #print("Did not get a message back from the arduino")
+        #print("Ghz:", ghzMessageAlpha[0] + ghzMessageAlpha[1]+ str(ep() - float(ghzMessageAlpha[2])))
+        #print(message)
         # The message that will get sent to the arduino
-        if d == 'r' and ep() - float(message[2]) < 2:
-            try:
-                if mode != {}:
-                    if mode["mode"] == 'manual':
-                        sockArd.sendto(message[1], arduino_address)
-                    else:
-                        print("Mode is not manual")
+        if True: #ep() - float(message[2]) - secondsOffset < 10:
+          #try:
+            if "mode" in mode:
+              if ep() - float(message[2]) - secondsOffset < 4:
+                if mode["mode"] == 'manual':
+                  #print(message)
+                  sockArd.sendto(message[1], arduino_address)
+                  if(d != 'r'):
+                    print("Unsuccessful message sent to arduino")
+                  else:
+                    #print("GhzTimeDiff: " + str(ep() - float(ghzMessageAlpha[2]) - secondsOffset))
+                    #print("HamTimeDiff: " + str(ep() - float(hamMessageBravo[2]) - secondsOffset))
+                    #print("Attempted message to arduino: " + message[1])
+                    pass
                 else:
-                    print("mode is not yet been set to manual")
-            except (BlockingIOError, socket.timeout, OSError):
-                print("couldn't send message to arduino")
+                  pass
+              else:
+                  sockArd.sendto('0,0,0,0,0,0,0,0,0,0', arduino_address)
+            else:
+              print("mode is not yet been set to manual")
+          #except:
+          #  print("couldn't send message to arduino")
         else:
-            sleep(.1)
-            sockArd.sendto(message[1], arduino_address)
-        
+          sleep(.1)
+          sockArd.sendto(data, arduino_address)
+          print("timeStamp not less than 3")
+          pass
+          #sockArd.sendto(message[1], arduino_address) 
 
 def getDataFromDeepstream():
     global mode
     while True:
         try:
             m = get('mode')
-            if type(m) == dict:
+            if type(m) == dict and "mode" in m:
                 mode = m
-                print(mode)
+                #print(mode)
         except:
             pass
         sleep(.8)
         
+if os.getenv("roverType") is not None:
+  if os.environ["roverType"] == "rover":
+    # Create a TCP/IP socket for the base
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind the socket to the port
+    server_address = ('0.0.0.0', 5001)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    # If binding to the socket fails, then we need to reset socket 5001 by killing the processId binded to 5001:
+    try:
+      sock.bind(server_address)
+    except:
+      o = Popen(["lsof", "-t", "-i", "udp:5001"], stdout=PIPE, stderr=PIPE).communicate()[0] 
+      print('PROCESSNUM: '+ o)
+      os.kill(int(o), 9)
+      sleep(3)
+      sock.bind(server_address) 
+      print("SOCKET 5001 WAS RESET. KILLED PID "+o)
+      pass
 
-if os.environ["roverType"] == "rover":
     t1 = Thread(target = connectionToGhzAlpha)
     t2 = Thread(target = connectionToHamBravo)
     t3 = Thread(target = sendLatestMessage)
@@ -172,3 +248,6 @@ if os.environ["roverType"] == "rover":
     t3.start()
 
     t5.start()
+else:
+    print("You need to set the environment variable roverType=\"rover\"")
+
