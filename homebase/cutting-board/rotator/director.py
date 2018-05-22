e767463d2cf2ae47ff07af766ffb6438df94155a
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2018-05-21 21:43:44 -0700

    antenna director: saving my place, saving my mind, also other things

--- homebase/cutting-board/rotator/director.py
+++ homebase/cutting-board/rotator/director.py
@@ -1,25 +1,30 @@
-import time
+print("starting director")
+from time import sleep
 import sys
 import math
 import numpy as np
-from socket import *
+import socket
 from struct import *
 from threading import Thread
 from decimal import Decimal
+from deepstream import *
 #import pyserial
 import datetime
 import subprocess
+import re
+import requests
+
 
 
 # gnss connection info
-__antenna_gps_address = ("192.168.1.38", 3777)
-__antenna_gps_socket = socket(AF_INET, SOCK_DGRAM)
+__antenna_gps_address = "192.168.1.38", 3777
+__antenna_gps_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 __antenna_gps_socket.settimeout(0.5)
 
-__rover_gps_address = ("place holder",9999)
-__rover_gps_socket = socket(AF_INET, SOCK_DGRAM)
-__antenna_gps_socket.settimeout(0.5)
-
+#__rover_gps_address = ("place holder",9999) #gonna grab this from deepstream
+#__rover_gps_socket = socket(AF_INET, SOCK_DGRAM)
+#__antenna_gps_socket.settimeout(0.5)
+__rover_gps = ('', '')
 
 __antenna_gps = (None, None)
 __rover_gps = (None, None)
@@ -30,13 +35,66 @@ __clockwise = None
 __deltaDirection = 0
 __paused = False
 __stop = False
-time.sleep(3)
 
 def getAntennaGPS():
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(('', 7075))
+            s.listen(2)
+            break
+        except:
+            print("Waiting For Connection")
+            sleep(2)
+
+
+    while True:
+        try:
+            conn, address = s.accept()
+            print("Connection from", address)
+            break
+        except:
+            print("ERROR CONNECTING")
+            sleep(3)
+        
+    pattern = re.compile('(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)')
+    print("Waiting for GPS Lock...")
+
+    while True:
+        try:
+            data = conn.recv(2048)
+            print(data)
+            m = re.match(pattern, data)
+            if m:
+                payload = {"body":[{"topic": "record", "action":"write", "recordName": "rover/gps", 
+                "data": {"lat": float(m.group(3)), "lon": float(m.group(4)),
+                "altitude": float(m.group(5)), "fix": (True if (int(m.group(6)) > 0) else False),
+                "nos": int(m.group(7)), "sdn":float(m.group(8)), 
+                "sde": float(m.group(9)), "sdu":float(m.group(10)),
+                "sdne":float(m.group(11)), "sdeu":float(m.group(12)),
+                "sdun":float(m.group(13)), "age":float(m.group(14)), 
+                "ratio":float(m.group(15)) }} ]}
+                __antenna_gps = (payload["lat"] + payload["lon"])
+                sys.stdout.write(payload["lat"] + payload["lon"])   
+                #sys.stdout.write(m.group(1) + ' ' + m.group(2) + ' ' + m.group(2) + ' '
+                #+ m.group(3) + ' ' + m.group(4) + m.group(5) + ' ' + m.group(6) + ' ' 
+                #+ m.group(7) + ' ' + m.group(8) + ' ' + m.group(9) + ' ' + m.group(10) + ' ' 
+                #+ m.group(11) + ' '+ m.group(12) + ' '+ m.group(13) + ' ' + m.group(14) + ' '
+                #+ m.group(15) + ' '+ '\n')
+                    
+            else:
+                print("No valid regex data")
+                s.close()
+                break
+
+            print(data)
+        except KeyboardInterrupt:
+            pass
 
     return __antenna_gps
 
 def getRoverGPS():
+    __rover_gps = (get('gps')['lat'], get('gps')['lon'])
     return __rover_gps
 
 def setTargetHeading(__antena_gps, __rover_gps):
@@ -78,7 +136,7 @@ def setHeading():
         heading
     '''
     print("checking heading")
-    __antenna_heading = subprocess.check_output(["python3", "test.py"])
+    __antenna_heading = subprocess.check_output(["python3", "IMU_Acc_Mag_Gyro.py"])
     return __antenna_heading
 def setHeadingDifference(__antenna_heading, __targetHeading):
     '''
@@ -119,28 +177,19 @@ def setShouldTurnClockwise():
     b = myDict[min(myDict.keys())]
     __clockwise = True if b > 0 else False
 
-def main():
-
-
-    ARDUINO_PORT = "/dev/ttyUSB0"
-    BAUD_RATE = 9600
-    #ard = Serial.serial(ARDUINO_PORT, BAUD_RATE)
-    #ard.open()
-    minPot = 0
-    maxPot = 1023
-    valPot = 511
-    MARGIN_OF_ERROR = 5 #error of 5 degrees
-    #rover_gps = __rover_gps_socket.recv(64)
-    #get antenna gps point from gnss
-    #antenna_gps = __antenna_gps_socket.recv(64) #grabs gps being sent from gnss module over network socket
-    testheading = setHeading()
-    print("got heading")
-    print(testheading)
+#ard = Serial.serial(ARDUINO_PORT, BAUD_RATE)
+#ard.open()
+#rover_gps = __rover_gps_socket.recv(64)
+#get antenna gps point from gnss
+#antenna_gps = __antenna_gps_socket.recv(64) #grabs gps being sent from gnss module over network socket
+print("running")
+testheading = setHeading()
+print("got heading")
+print(testheading)
 
 
 
-    #setHeading()
-    #setHeadingDifference()
-    #setDeltaDirection()
-    #setShouldTurnClockwise()
-    return heading
+#setHeading()
+#setHeadingDifference()
+#setDeltaDirection()
+#setShouldTurnClockwise()
