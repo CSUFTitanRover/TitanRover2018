bb8f65a7fa2cfbf6e0dcc3c5a416ee16381d6d14
rawalshree <rawalshreepal000@gmail.com>
2018-05-22 20:34:13 -0700

    new change

--- rover/core/servers/roverPoke/poke.py
+++ rover/core/servers/roverPoke/poke.py
@@ -7,15 +7,15 @@ sleep(8.5)
 
 while True:
     try:
-        ser = Serial('/dev/serial/by-id/usb-Silicon_Labs_poke_3387-if00-port0', 9600)
+        ser = Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0', 9600)
     except:
         print("cannot connect to serial device.")
-    poke = get('poke', 'localhost')
+    #poke = get('poke', 'localhost')
     if 'poke' in poke:
         if poke['poke'] == True:
             print('POKE!!')
             ser.write('1')
             sleep(0.05)
-            post({ 'poke': False }, 'poke', 'localhost')
+            #post({ 'poke': False }, 'poke', 'localhost')
             sleep(0.05)
     sleep(0.05)
