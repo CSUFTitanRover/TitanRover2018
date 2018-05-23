f30c78a947d0f7335be0ed602c8335006669397f
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2018-05-23 04:58:48 -0700

    -updated send.py and rec.py in the relay tests folder -send.py and rec.py now implement struct.pack to send data -send.py and rec.py now implement start and stop bytes to synchronize data flow -putRF and getRF are now self-contained functions that can copied and used wherever, i think -other things

--- homebase/cutting-board/relay/tests/rec.py
+++ homebase/cutting-board/relay/tests/rec.py
@@ -1,22 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #receives data from a uart rf module and prints what it gets
 import serial
-import time
+from time import sleep
+ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=None)
+def pnw(): #testing function reads all from buffer and prints its length and contents
+    n = ser.read_all()
+    print(len(n))
+    print(n)
+    sleep(0.5)
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
-# the library, though you import "serial," is installed using pip install pyserial
-# read example
-
-ser.setDTR(True) #if the extra pins on the ttl usb are connected to m0 & m1 on the ebyte module
-ser.setRTS(True) #then these two lines will send low logic to both which puts the module in transmit mode 0
+def getRF(rf_uart, size_of_payload): #added argument to make it more function-like
+    ser.setDTR(True) #if the extra pins on the ttl usb are connected to m0 & m1 on the ebyte module
+    ser.setRTS(True) #then these two lines will send low logic to both which puts the module in transmit mode 0
+    while True:
+        n = rf_uart.read(1) #read bytes one at a time
+        if n == b's': #throw away bytes until start byte is encountered
+            data = ser.read(size_of_payload) #read fixed number of bytes
+            n = ser.read(1) #the following byte should be the stop byte
+            if n == b'f':
+                print('success')
+                print(data)
+            else: #if that last byte wasn't the stop byte then something is out of sync
+                print("failure")
+    return data
 
 while True:
-    n = ser.read_all()
-    print(n)
-    time.sleep(.5)
-
-#serial write example:
-#while True:
-#    ser.write(0x11111111)
-#  print("sent\n")
-#   time.sleep(1)
+    #pass serial object to receive from
+    #and size of payload IN BYTES
+    serial_data = getRF(ser, 10) #returns a tuple of sent variables
