fe5b2e15b9f46e067d29264ce64cfe9c6d437763
Weffe <rogelio_negrete@live.com>
2018-05-31 00:34:38 -0700

    Add stopwatch fixes

--- homebase/user-interface/src/components/Stopwatch/index.js
+++ homebase/user-interface/src/components/Stopwatch/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import green from '@material-ui/core/colors/green';
 import red from '@material-ui/core/colors/red';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
+import cn from 'classnames';
+import { getClient } from '../../utils/deepstream';
+import DeepstreamRecordProvider from '../../utils/DeepstreamRecordProvider';
 
 const styles = theme => ({
   paper: {
@@ -33,23 +34,6 @@ const styles = theme => ({
   },
 });
 
-/** Adds leading zeros to a number, plus an ending character */
-const padZeros = (number, length, ending) => (`${number}`).padStart(length, '0') + ending;
-
-/** Formats a duration in milliseconds to a time string */
-const formatTime = (elapsed) => {
-  // Find total elapsed time and separate it into its components
-  let ms = elapsed;
-  let sec = Math.floor(ms / 1000);
-  ms %= 1000;
-  let min = Math.floor(sec / 60);
-  sec %= 60;
-  const hour = Math.floor(min / 60);
-  min %= 60;
-
-  return padZeros(hour, 2, ':') + padZeros(min, 2, ':') + padZeros(sec, 2, '.') + padZeros(ms, 3, '');
-};
-
 /**
  * Stopwatch component
  * Keeps track of elapsed mission time.
@@ -59,87 +43,61 @@ class Stopwatch extends Component {
     classes: PropTypes.object.isRequired,
   }
 
-  /**
-   * state:
-   *  time: displayed time value (string)
-   *  active: whether it is running (boolean)
-   *  startTime: when the time was last started/resumed (int)
-   *  accTime: how much time has accumulated (int)
-   */
-  state = { time: '00:00:00.000', active: false, startTime: 0, accTime: 0 }
+  state = { elapsedTime: 0, active: false }
 
-  componentDidMount() {
-    window.requestAnimationFrame(this.tick);
+  async componentDidMount() {
+    this.client = await getClient('homebase');
   }
 
-  /** Update the display time value */
-  tick = () => {
-    window.requestAnimationFrame(this.tick);
+  formatTime = value => moment().hour(0).minute(0).second(value)
+    .format('HH:mm:ss');
 
-    // Get current ms running
-    let ms = (new Date()).getTime();
-    if (!this.state.active) {
-      ms = 0;
-    }
-
-    // Find total elapsed time and update time string
-    const elapsed = ms - this.state.startTime + this.state.accTime;
-    this.setState({
-      time: formatTime(elapsed),
-    });
-  }
-
-  /** Activate/deactivate the timer when the start/stop button is pressed */
   handleStartAndStop = () => {
-    this.setState((prevState) => {
-      // Timer is stopped
-      let newStartTime = (new Date()).getTime();
-      let newAccTime = prevState.accTime;
+    const { active } = this.state;
 
-      if (prevState.active) {
-        // Find out the elapsed time on the timer and stop the interval
-        newAccTime = (new Date()).getTime() - prevState.startTime + prevState.accTime;
-        newStartTime = 0;
-      }
+    if (active) {
+      // we want to stop the stopwatch
+      this.client.event.emit('homebase/stopwatch:stop');
+    } else {
+      // we want to start the stopwatch
+      this.client.event.emit('homebase/stopwatch:start');
+    }
+  }
 
-      // Toggle active state
-      return { active: !prevState.active, startTime: newStartTime, accTime: newAccTime };
-    });
-  };
-
-  /** Clears all states */
   handleClear = () => {
-    this.setState({
-      active: false,
-      startTime: 0,
-      accTime: 0,
-    });
+    this.client.event.emit('homebase/stopwatch:clear');
+  }
+
+  handleNewPayload = (data) => {
+    this.setState(data);
   }
 
   render() {
     const { classes } = this.props;
-    const { active } = this.state;
+    const { active, elapsedTime } = this.state;
+
     return (
-      <Grid container spacing={16} alignItems="center">
-        <Grid item xs={2}>
-          <Typography variant="title" color="inherit">Mission Stopwatch</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Paper className={classes.paper}>
-            <Typography>{this.state.time}</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={1}>
-          <Button variant="raised" color="primary" className={classNames(classes.stopwatchTriggerButton, (active ? classes.stopButton : classes.startButton))} onClick={this.handleStartAndStop}>
-            {this.state.active ? 'Stop' : 'Start'}
-          </Button>
-        </Grid>
-        <Grid item xs={1}>
-          <Button variant="raised" color="default" onClick={this.handleClear}>
-            Clear
-          </Button>
-        </Grid>
-      </Grid>
+      <DeepstreamRecordProvider recordPath="homebase/stopwatch" onNewPayload={this.handleNewPayload}>
+        {() => (
+          <div className={classes.container}>
+            <Typography variant="title" color="inherit">Mission Stopwatch</Typography>
+            <div className={classes.paper}>
+              <Typography>{this.formatTime(elapsedTime)}</Typography>
+            </div>
+            <Button
+              variant="raised"
+              color="primary"
+              className={cn(classes.stopwatchTriggerButton, (active ? classes.stopButton : classes.startButton))}
+              onClick={this.handleStartAndStop}
+            >
+              {active ? 'Stop' : 'Start'}
+            </Button>
+            <Button variant="raised" color="default" onClick={this.handleClear}>
+              Clear
+            </Button>
+          </div>
+        )}
+      </DeepstreamRecordProvider>
     );
   }
 }
